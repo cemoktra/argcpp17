658b53af6f3d7e43fbcaed000624548e8e42b854
Bastian Schubert <b.schubert82@gmail.com>
2020-01-22 13:53:49 +0100

    fixed unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -465,7 +465,7 @@ TEST_F(parser_test, parse_posistionals)
        .add_positional(ANOTHER_KEY, DESC);
 
     args = {KEY, ANOTHER_KEY};
-    EXPECT_NO_THROW(sut.parse_vector(args));
+    EXPECT_THROW(sut.parse_vector(args), argcpp17_exception);
 
     args = {ANOTHER_KEY};
     EXPECT_THROW(sut.parse_vector(args), argcpp17_exception);
@@ -478,7 +478,7 @@ TEST_F(parser_test, parse_posistionals)
 
     sut.add_flag({ABBR}, DESC);
     args = {ABBR, KEY, ANOTHER_KEY};
-    EXPECT_NO_THROW(sut.parse_vector(args));
+    EXPECT_THROW(sut.parse_vector(args), argcpp17_exception);
 }
 
 TEST_F(parser_test, parse_values)
